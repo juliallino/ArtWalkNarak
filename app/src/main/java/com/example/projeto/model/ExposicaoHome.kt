dd59341c33cc86c2db7d4ffc72ad5ff075b915e3
Júlia Lino <linojulia189@gmail.com>
2024-10-04 02:30:36 -0300

    ajeitando o git

--- app/src/main/java/com/example/projeto/model/ExposicaoHome.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projeto.model
-
-data class ExposicaoHome(
-    val nomeExposicao: String,
-    //  val imagemExposicao: String por ser url
-    val imagemExposicao: Int,
-    val editExposicao: Int,
-    val descricaoExposicao: String
-)
